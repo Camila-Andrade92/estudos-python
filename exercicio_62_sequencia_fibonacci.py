3ad5118d30f61f61fb40b1e31a913b2459a968f1
Camila Andrade <gomescandrade@gmail.com>
2022-03-02 20:32:58 -0300

    Alteração variáveis

--- exercicio_62_sequencia_fibonacci.py
+++ exercicio_62_sequencia_fibonacci.py
@@ -1,19 +1,18 @@
 #Faça um programa que leia um número inteiro e mostre os primeiros elementos de 
 #uma sequência fibonnacci
 
-numero=int(input("Quantos termos você quer mostrar? "))
-numero1=0
-numero2=1
-numero3=(numero1+numero2)
+n=int(input("Quantos termos você quer mostrar? "))
+n1=0
+n2=1
+n3=(n1+n2)
 print("--"*30)
 contador=3#pq já tem os valores de n1, n2, n3
 print("A sequência febonacci do número escolhido é:")
-while contador <= numero:
+while contador <= n:
     contador += 1
-    print("{} -> {} -> ".format(numero1,numero2), end="")
-    print("{}".format(numero3), end="")
-    nnumero1=numero2#faz com que os n eles andem e somam os próximos valores
-    nnumero2=numero3
+    print("{} -> {} -> ".format(n1,n2), end="")
+    print("{}".format(n3), end="")
+    n1=n2#faz com que os n eles andem e somam os próximos valores
+    n2=n3
 
 print("Fim!")
-
